85ba9437a9ac44aa2f720a804c47cd6c62d6c25d
unknown <biancapeleao@gmail.com>
2023-02-09 20:35:37 -0300

    add algumas validações

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
 'use strict'
 const buttonCalcular = document.getElementById('calcular')
 
-
-
 const calcularTabuada = function () {
-    let alvo = document.getElementById("resultadoList");
-    alvo.innerText = "";
-  //  let tabuada = Number(String(multiplicando).replace(',', '.'));
-  //  let maxContador = Number(String(maxMultiplicador).replace(',', '.'));
+
+    let cleanDivResultado = document.getElementById("resultadoList");
+    cleanDivResultado.innerText = "";
+
     let status = true;
     let cont = 0;
     let resultado;
 
     const inputMultiplicando = document.getElementById('multiplicando').value.replace(',', '.')
     const inputMultiplicador = document.getElementById('multiplicador').value.replace(',', '.')
-
+    const textareaResultadoList = document.getElementById('resultadoList')
 
 
     if (inputMultiplicando == 0 || inputMultiplicador == 0)
-        status = false;
+    textareaResultadoList.innerHTML += `O multiplicando e o máximo multiplicador não pode ser zero`;
     else if (isNaN(inputMultiplicando) || isNaN(inputMultiplicador))
-        status = false;
+    textareaResultadoList.innerHTML += `Digite apenas números`;
     else {
         while (cont <= inputMultiplicador) {
             resultado = inputMultiplicando * cont;
-            //document.getElementById('resultadoList').value = inputMultiplicando +'x'+ cont + '=' + resultado 
-            console.log(inputMultiplicando +'x'+ cont + '=' + resultado);
-            const textareaResultadoList = document.getElementById('resultadoList')
-            textareaResultadoList.innerHTML += `${inputMultiplicando} x ${cont} = ${resultado} <br/>`; 
-            //console.log(resultado)
-            //cont = cont + 1;
+            //const textareaResultadoList = document.getElementById('resultadoList')
+            textareaResultadoList.innerHTML += `${inputMultiplicando} x ${cont} = ${resultado} <br/>`;
             cont++;
 
-            //cont += 1;
         }
-        
+
     }
-    
+
     return status;
-    
+
 };
 
 buttonCalcular.addEventListener('click', calcularTabuada)
